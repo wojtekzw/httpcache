d3e10afece8b0be75489a95c8f1bd4ec11346b75
Wojciech Zwiefka <wojtekz@wp.pl>
2016-12-12 09:40:03 +0100

    debug version of limiting cache size on disk

--- diskcache/diskcache.go
+++ diskcache/diskcache.go
@@ -9,11 +9,15 @@ import (
 	"encoding/hex"
 	"github.com/wojtekzw/diskv"
 	"io"
+	"github.com/golang/glog"
+	"github.com/bluele/gcache"
 )
 
 // Cache is an implementation of httpcache.Cache that supplements the in-memory map with persistent storage
 type Cache struct {
 	d *diskv.Diskv
+	// key cache for maintaining usage of keys, when not used will help do Erase real data from d
+	kc gcache.Cache
 }
 
 // Get returns the response corresponding to key if present
@@ -23,6 +27,8 @@ func (c *Cache) Get(key string) (resp []byte, ok bool) {
 	if err != nil {
 		return []byte{}, false
 	}
+	// increment key usage
+	c.kc.Get(key)
 	return resp, true
 }
 
@@ -30,32 +36,55 @@ func (c *Cache) Get(key string) (resp []byte, ok bool) {
 func (c *Cache) Set(key string, resp []byte) {
 	key = keyToFilename(key)
 	c.d.WriteStream(key, bytes.NewReader(resp), true)
+	// increment key usage
+	c.kc.Set(key,struct{}{})
 }
 
 // Delete removes the response with key from the cache
 func (c *Cache) Delete(key string) {
 	key = keyToFilename(key)
 	c.d.Erase(key)
+	// remove from key cache
+	c.kc.Remove(key)
 }
 
 func keyToFilename(key string) string {
 	h := md5.New()
 	io.WriteString(h, key)
-	return hex.EncodeToString(h.Sum(nil))
+	s := hex.EncodeToString(h.Sum(nil))
+
+	glog.Infof("HTTPCache::keyToFilename: key: %s, filename: %s",key,s)
+	return s
 }
 
 // New returns a new Cache that will store files in basePath
 func New(basePath string) *Cache {
+	d := diskv.New(diskv.Options{
+		BasePath:     basePath,
+		CacheSizeMax: 100 * 1024 * 1024, // 100MB
+	})
+	kc := gcache.New(20000).LRU().EvictedFunc(func(key, value interface{}) {
+		glog.Infof("evicted key: %v", key)
+		d.Erase(key.(string))
+	}).Build()
+
 	return &Cache{
-		d: diskv.New(diskv.Options{
-			BasePath:     basePath,
-			CacheSizeMax: 100 * 1024 * 1024, // 100MB
-		}),
-	}
+		d: d,
+		kc: kc, }
 }
+
 
 // NewWithDiskv returns a new Cache using the provided Diskv as underlying
 // storage.
 func NewWithDiskv(d *diskv.Diskv) *Cache {
-	return &Cache{d}
+	kc := gcache.New(20000).LRU().EvictedFunc(func(key, value interface{}) {
+		glog.Infof("evicted key: %v", key)
+		d.Erase(key.(string))
+	}).Build()
+
+	return &Cache{
+		d: d,
+		kc: kc, }
 }
+
+
